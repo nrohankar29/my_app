d5bea662ba15bf0ffd356f1401d8d6f9c3b78ab7
Ganesh Khakare <ganeshkhakare@teradata.com>
2018-08-13 12:30:08 +0530

    2nd Version

--- my-app2/src/main/java/com/mycompany/app/B.java
+++ my-app2/src/main/java/com/mycompany/app/B.java
@@ -8,6 +8,6 @@ public class B
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! - B" );
+        System.out.println( "Hello World! - 0.0.2" );
     }
 }
