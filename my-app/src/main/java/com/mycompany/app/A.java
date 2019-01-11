03118a06fcba6fa0c0a6e918c6a0f8a74beccc56
Nikhil Rohankar <nikhil.rohankar@hitachivantara.com>
2019-01-11 14:35:35 +0530

    first commit

---
+++ my-app/src/main/java/com/mycompany/app/A.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class A 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! - version - 0.0.9" );
+    }
+}
---
+++ my-app2/src/main/java/com/mycompany/app/B.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class B 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! - feature - 0.0.4" );
+    }
+}
